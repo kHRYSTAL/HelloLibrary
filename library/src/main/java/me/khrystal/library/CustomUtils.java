16da9665ebb14a6b20a409fd00f37aa2c06c95e9
kHRYSTAL <723526676@qq.com>
2016-01-26 23:55:18 +0800

    add library

---
+++ library/src/main/java/me/khrystal/library/CustomUtils.java
@@ -0,0 +1,10 @@
+package me.khrystal.library;
+
+/**
+ * Created by ASUS on 2016/1/26.
+ */
+public class CustomUtils {
+    public static String getMyHero(){
+        return "kHRYSTAL";
+    }
+}
--- sample/src/main/java/me/khrystal/hellolibrary/MainActivity.java
+++ sample/src/main/java/me/khrystal/hellolibrary/MainActivity.java
@@ -2,8 +2,11 @@ package me.khrystal.hellolibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import me.khrystal.library.CustomUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Library",CustomUtils.getMyHero());
     }
 
     @Override
